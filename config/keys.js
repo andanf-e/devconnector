d6be0aee553d86981096b5a3f29a2154cc19916d
andanfe@gmail.com <andanfe@gmail.com>
2019-08-28 17:36:54 +0300

    added user model + added basic routes for login and registration + added basic validation for each route

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://s_oux:dynamo@mern-sandbox-njavf.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://s_oux:dynamo@mern-sandbox-njavf.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secretkey"
 };
--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
 const db = require("./config/keys").mongoURI;
 
-const app = express();
 // Connect to DB
 mongoose
   .connect(db)
   .then(() => console.log("mongodb connection successful"))
   .catch(error => console.log(error));
 
-app.get("/", (req, res) => res.send("hello from kek server.js"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport.js")(passport);
 
 // Use routes
 app.use("/api/users", users);
