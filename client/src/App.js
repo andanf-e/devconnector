3c1674d3cef44054691c0fa58090a1409164d60e
andanfe@gmail.com <andanfe@gmail.com>
2019-08-30 17:10:12 +0300

    completed posts route with likes and comments realization +  added initial react client insertion

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Landing from "./components/Layout/Landing";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
