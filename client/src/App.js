9634f6af0fc3af425511df4f3b2f32050ccbc8f0
andanfe@gmail.com <andanfe@gmail.com>
2019-09-02 16:23:32 +0300

    implemented redux for login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,53 @@
 import React, { Component } from "react";
+import jwt_decode from "jwt-decode";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./App.css";
 
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import setAuthToken from "./utils/setAuthToken";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import store from "./store";
+
+if (localStorage.jwtToken) {
+  // set auth token header
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuth
+  store.dispatch(setCurrentUser(decoded));
+  // check for expired token
+  const currentTime = Date.now() / 1000;
+  if(decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+  
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
+
 export default App;
---
+++ client/src/actions/authActions.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+
+import setAuthToken from "../utils/setAuthToken";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post("/api/users/register", userData)
+    .then(result => history.push("/login"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      //   Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+export const logoutUser = () => dispatch => {
+  // remove token
+  localStorage.removeItem("jwtToken");
+  // remove auth header
+  setAuthToken(false);
+  // set current user to {} and isAuth to false
+  dispatch(setCurrentUser({}));
+};
