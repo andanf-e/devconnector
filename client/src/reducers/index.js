6a9ba08daeafc0d91f644bbe7d3edf19d379f35a
andanfe@gmail.com <andanfe@gmail.com>
2019-09-06 15:26:15 +0300

    added post feed display functionality with comment functionality

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import profileReducer from "./profileReducer";
 import errorReducer from "./errorReducer";
+import postReducer from "./postReducer";
 
 export default combineReducers({
   auth: authReducer,
   profile: profileReducer,
-  errors: errorReducer
+  errors: errorReducer,
+  post: postReducer
 });
