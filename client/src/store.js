0ba7c73fffd76ab2a8e82be5a5007cf0f445775b
andanfe@gmail.com <andanfe@gmail.com>
2019-09-27 13:10:07 +0300

    another try to fix failure

--- client/src/store.js
+++ client/src/store.js
@@ -6,6 +6,7 @@ import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+
 // new
 // const composeEnhancers =
 //   process.env.NODE_ENV === 'development'
@@ -23,13 +24,13 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    process.env.NODE_ENV === 'development'
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
           window.__REDUX_DEVTOOLS_EXTENSION__()
-      : null
-  )
+      )
+    : compose(applyMiddleware(...middleware))
 );
 
 export default store;
